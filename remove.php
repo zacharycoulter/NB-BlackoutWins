4e9190b885a6c58dd0c0f79912b413add7d44417
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2018-09-15 22:10:02 -0400

    Added remove.php to reduce BO wins by one

---
+++ remove.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_SERVER["HTTP_NIGHTBOT_USER"])) {
+    parse_str($_SERVER['HTTP_NIGHTBOT_USER'], $nightbotHeader);
+
+    if ($nightbotHeader['userLevel'] == "moderator" || $nightbotHeader['userLevel'] == "owner") {
+        $wins = (int)file_get_contents('./wins.txt');
+        $wins--;
+
+        echo $wins;
+        file_put_contents('./wins.txt', $wins);
+        return;
+    }
+}
+
+header('HTTP/1.0 403 Forbidden');
+die('You are not allowed to access this file.');
